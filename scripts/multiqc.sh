cf37d2da34dbffa645d5fe637116f16264d6818e
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2025-06-25 11:50:02 +0200

    Update and rename multiqc to multiqc.sh

--- scripts/multiqc.sh
+++ scripts/multiqc.sh
@@ -1,12 +1,8 @@
 #This PBS script runs MultiQC to generate a consolidated quality control report. It:
-
-Loads the MultiQC module
-
-Scans the /nlustre/users/muremb/project/xaa directory for QC output files
-
-Produces a single HTML report summarizing all results
-
-Optimized for HPC environments using 24 threads.
+#Loads the MultiQC module
+#Scans the /nlustre/users/muremb/project/xaa directory for QC output files
+#Produces a single HTML report summarizing all results
+#Optimized for HPC environments using 24 threads.
 
 #!/bin/bash
 #PBS -l nodes=1:ppn=24
